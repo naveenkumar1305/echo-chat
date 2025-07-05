47827fa70f276b79ff5f28bdd41da235de2125d0
naveen kumar <nave22063.it@rmkec.ac.in>
2025-07-05 14:20:39 +0530

    Refactor: Moved Firebase config to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBgprZkrPIhQacvTiGDAHYqHxX8haj93eA",
-  authDomain: "echochat-8e15b.firebaseapp.com",
-  projectId: "echochat-8e15b",
-  storageBucket: "echochat-8e15b.appspot.com",
-  messagingSenderId: "247133613432",
-  appId: "1:247133613432:web:38298fe9cba4091453ec8d"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId:  process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
